acf4e47932d6fdaffc6d1ee5ad1ead7a1c8a1e54
Jeffrey Lui <jlui820@gmail.com>
2020-05-29 08:55:57 -0700

    Updates

---
+++ precache-manifest.0ecc9829175e1434a4b7f5b059facd1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "708771c8a2b89b2404e1198f4a73fbb1",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "1e8a905fdf233d73e981",
+    "url": "/monster/static/css/main.20af59eb.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monster/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e8a905fdf233d73e981",
+    "url": "/monster/static/js/main.7aae3189.chunk.js"
+  },
+  {
+    "revision": "247ddf520f3ce9235187",
+    "url": "/monster/static/js/runtime-main.69462feb.js"
+  }
+]);
